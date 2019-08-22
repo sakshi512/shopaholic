e82a9cd89db2666a4acd0e9c06450ec7f9443a41
sakshiaggarwal <sakshi.agg.512@gmail.com>
2019-08-22 16:22:04 +0530

    Sakshi| Project strucuture

---
+++ src/main/java/com/shop/Shopaholic/ShopaholicApplication.java
@@ -0,0 +1,13 @@
+package com.shop.Shopaholic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopaholicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopaholicApplication.class, args);
+	}
+
+}
