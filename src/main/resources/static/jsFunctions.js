1a10f9458faca9045c9067d4bb12b9812f0129b1
anusingh <1234>
2019-08-23 15:19:20 +0530

    Anu| Story-5| Added cart functionality on index page

---
+++ src/main/resources/static/jsFunctions.js
@@ -0,0 +1,7 @@
+///
+function addtoCart() {
+    var prevCount = parseInt(document.getElementById("cart").innerHTML);
+    prevCount += 1;
+    document.getElementById("cart").innerHTML = prevCount.toString();
+
+}
