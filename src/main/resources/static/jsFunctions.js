672be0cf069fc15ae848b3f3d6dae8407f6c34ce
sakshiaggarwal <sakshi.agg.512@gmail.com>
2019-08-26 11:34:42 +0530

    Anu,Sakshi|Story-5|Changes for add to cart functionality in ajax function

--- src/main/resources/static/jsFunctions.js
+++ src/main/resources/static/jsFunctions.js
@@ -1,13 +1,14 @@
 /// Add a product to the Cart
 function addtoCart(userId, productId) {
-    var prevCount = parseInt(document.getElementById("cart").innerHTML);
-    prevCount += 1;
-    document.getElementById("cart").innerHTML = prevCount.toString();
 
     var ajaxRequest = new XMLHttpRequest();
     ajaxRequest.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
            // alert(this.responseText);
+
+            var prevCount = parseInt(document.getElementById("cart").innerHTML);
+            prevCount += 1;
+            document.getElementById("cart").innerHTML = prevCount.toString();
         }
     };
     ajaxRequest.open("GET", "/api/addCart?productId="+productId+"&userId="+userId, true);
