770ef698a22c781050f31f4deb989372dc7f276d
sakshiaggarwal <sakshi.agg.512@gmail.com>
2019-08-27 16:02:04 +0530

    Sakshi,Sheetal|Feedback fixes|Incorporated changes for error message of invalid login

--- src/main/java/com/shop/Shopaholic/controllers/LoginController.java
+++ src/main/java/com/shop/Shopaholic/controllers/LoginController.java
@@ -6,10 +6,10 @@ import com.shop.Shopaholic.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
@@ -29,10 +29,10 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String doUserLogin(@ModelAttribute("user") LoginEntity user, HttpSession httpSession, ModelMap modelMap) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public String doUserLogin(@ModelAttribute("user") LoginEntity user, HttpSession httpSession, RedirectAttributes attributes) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         Optional<UserEntity> validUser = loginService.validateUser(user);
         if(!validUser.isPresent()){
-            modelMap.addAttribute("errorMsg","Invalid Credentials!! Kindly check username/password");
+            attributes.addFlashAttribute("errorMsg","Invalid Credentials!!");
             return "redirect:login";
         }
 
